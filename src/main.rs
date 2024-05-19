82db07bf9a92e4e91e3cacd9cfec7c6c4bc985e3
zkthernum <kevin91cooland@gmail.com>
2024-05-19 13:51:52 +0800

    review server logic

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ use tokio::{
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    // The Server's logic
+    /*
+       1. bind address.
+       2. accept conn.
+       3. using `tcp.read()` to read data into local buffer.
+       4. using `tcp.write()` to write data from buffer into conn.
+    */
     let server = TcpListener::bind("127.0.0.1:42069").await?;
     let (mut tcp, _) = server.accept().await?;
     let mut buffer = [0u8; 16];
