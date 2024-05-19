d8c093ee63ebfb400331d403d46d359c9a021596
zkthernum <kevin91cooland@gmail.com>
2024-05-19 14:42:37 +0800

    reconstruct using stream and sink

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 #![allow(unused)]
 #![allow(dead_code)]
+use futures::{SinkExt, StreamExt};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpListener,
 };
+use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -17,19 +19,12 @@ async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:42069").await?;
     loop {
         let (mut tcp, _) = server.accept().await?;
-        let mut buffer = [0u8; 16];
-        loop {
-            let n = tcp.read(&mut buffer).await?;
-            if n == 0 {
-                break;
-            }
-            // Convert to String.
-            let mut line = String::from_utf8(buffer[..n].to_vec())?;
-            line.pop();
-            line.pop();
-            line.push_str(" ❤\n");
-            // Convert back to Byte slice.
-            let _ = tcp.write(line.as_bytes()).await?;
+        let (reader, writer) = tcp.split();
+        let mut stream = FramedRead::new(reader, LinesCodec::new());
+        let mut sink = FramedWrite::new(writer, LinesCodec::new());
+        while let Some(Ok(mut msg)) = stream.next().await {
+            msg.push_str(" ❤");
+            sink.send(msg).await?;
         }
     }
     Ok(())
